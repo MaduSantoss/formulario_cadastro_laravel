84f61aa70b5108dbba7ca6ada818efb7d7a5780e
MaduSantoss <madusantoss1510@gmail.com>
2025-08-02 21:29:37 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Requisitos
+
+* PHP 8.2 ou superior
+* Composer
+* Node.js 22 ou superior
+
+## Como rodar o projeto baixado
+
+Duplicar o arquivo ".env.example" e renomear para ".env".
+
+Instalar as dependências do PHP:
+
+composer install
+
+Gerar a chave:
+
+php artisan key:generate
+
+Iniciar o projeto criado com Laravel:
+
+php artisan serve
+
+Acessar o conteúdo padrão do Laravel:
+
+[Acesse o site](http://127.0.0.1:8000)
+
+Instalar as dependências do Node.js:
+
+npm install
+
+## Sequencia para criar o projeto
+
+Criar o projeto com Laravel:
+
+composer create-project laravel/laravel .
+
+Iniciar o projeto criado com Laravel.;
+
+php artisan serve
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(Request $request)
+    {   
+        try{
+        
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect()->route('user.create')->with('success', 'Usuário cadastrado com sucesso');
+        
+        }catch(Exception $e){
+            return back()->withInput()->with('error', 'Usuário não cadastrado');
+        }
+    }
+}
